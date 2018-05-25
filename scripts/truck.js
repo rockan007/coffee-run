fe011c983225fa80b62d634d246456f67f9ce9ab
rockan007 <rockan007@gmail.com>
2018-05-25 10:21:07 +0800

    first comment

---
+++ scripts/truck.js
@@ -0,0 +1,28 @@
+(function (window) {
+    var App = window.App || {};
+
+    function Truck(truckId, db) {
+        this.truckId = truckId;
+        this.db = db;
+    }
+    //添加订单
+    Truck.prototype.createOrder=function(order){
+        console.log('Adding order for '+order.emailAddress);
+        this.db.add(order.emailAddress,order);
+    }
+    //删除订单
+    Truck.prototype.deliverOrder=function(customerId){
+        console.log('Delivering order for '+customerId);
+        this.db.remove(customerId);
+    }
+    //打印订单
+    Truck.prototype.printOrders=function(){
+        var customerIdArray=Object.keys(this.db.getAll());
+        console.log('Truck #'+this.truckId+' has pending orders:');
+        customerIdArray.forEach(function(id){
+            console.log(this.db.get(id));
+        }.bind(this))
+    }
+    App.Truck = Truck;
+    window.App = App;
+})(window)
